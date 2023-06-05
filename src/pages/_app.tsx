8b8d43b971952c0ed8d2ccf4583b66bc34c554b4
Marcelo Kopmann <marcelokopmann@gmail.com>
2023-06-05 04:29:34 -0300

    feat: add dynamic og image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,36 @@ import { AppProps } from 'next/app'
 import '../styles/index.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const title = 'RepoGPT'
+  const description =
+    "Merge github's repository files into text to send it to GPT API or to copy and paste it into ChatGPT."
+  const keywords = 'gpt, openai, github, repository, merge, text, chat, chatgpt, repogpt'
+  const author = 'Mark Kop'
+  const imageUrl = 'https://repo-gpt-black.vercel.app/og.png'
+  const url = 'https://repo-gpt-black.vercel.app/'
+  const twitterHandle = '@HeyMarkKop'
+
   return (
     <>
       <Head>
         <title>RepoGPT</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={imageUrl} />
+        <meta property="og:url" content={url} />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content={twitterHandle} />
+        <meta name="twitter:creator" content={twitterHandle} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={imageUrl} />
+        <meta name="twitter:image:alt" content={title} />
+        <meta name="twitter:url" content={url} />
       </Head>
       <Component {...pageProps} />
     </>
---
+++ src/scripts/screenshot.js
@@ -0,0 +1,64 @@
+const puppeteer = require('puppeteer')
+require('dotenv').config()
+//
+;(async () => {
+  // Take the urls from the command line
+  var args = process.argv.slice(2)
+
+  try {
+    // launch a new headless browser
+    const browser = await puppeteer.launch({ headless: 'new' })
+
+    // loop over the urls
+    for (let i = 0; i < args.length; i++) {
+      // check for https for safety!
+      if (args[i].includes('http://')) {
+        const page = await browser.newPage()
+
+        // set the viewport size
+        await page.setViewport({
+          width: 1200,
+          height: 630,
+          deviceScaleFactor: 1
+        })
+
+        // tell the page to visit the url
+        await page.goto(args[i])
+
+        // fill open api key just for the screenshot
+        await page.type('#openai-api-key', Array(64).fill('a').join(''))
+
+        if (process.env.GITHUB_TOKEN) {
+          console.log('Github token found, using it to make requests.')
+          await page.type('#github-token', process.env.GITHUB_TOKEN)
+        }
+
+        // click on the button
+        await page.click('#save-repo-url')
+
+        // wait
+        await page.waitForSelector('input[type="checkbox"]')
+
+        // click on the 6th checkbox
+        const checkboxes = await page.$$('input[type="checkbox"]')
+        await checkboxes[5].click()
+
+        // wait
+        await page.waitForNetworkIdle()
+
+        // take a screenshot and save it in the screenshots directory
+        await page.screenshot({ path: `./public/og.png` })
+
+        // done!
+        console.log(`✅ Screenshot of ${args[i]} saved!`)
+      } else {
+        console.error(`❌ Could not save screenshot of ${args[i]}!`)
+      }
+    }
+
+    // close the browser
+    await browser.close()
+  } catch (error) {
+    console.log(error)
+  }
+})()
