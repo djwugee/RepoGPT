8710a27210de8f14bf027b03bf3a5aff4d35709f
Marcelo Kopmann <16388408+Markkop@users.noreply.github.com>
2023-06-05 04:52:34 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,8 +8,8 @@ function MyApp({ Component, pageProps }: AppProps) {
     "Merge github's repository files into text to send it to GPT API or to copy and paste it into ChatGPT."
   const keywords = 'gpt, openai, github, repository, merge, text, chat, chatgpt, repogpt'
   const author = 'Mark Kop'
-  const imageUrl = 'https://repo-gpt-black.vercel.app/og.png'
-  const url = 'https://repo-gpt-black.vercel.app/'
+  const imageUrl = 'https://repogpt.markkop.dev/og.png'
+  const url = 'https://repogpt.markkop.dev'
   const twitterHandle = '@HeyMarkKop'
 
   return (
